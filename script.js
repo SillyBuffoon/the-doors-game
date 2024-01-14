4d95328603933406b5844f02f6f88b47ed6c055d
SillyBuffoon <156584804+SillyBuffoon@users.noreply.github.com>
2024-01-14 14:25:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,20 +20,34 @@ document.addEventListener('DOMContentLoaded', function () {
             var randomnumber = getRandomInt(3);
             var doorNumber = parseInt(this.getAttribute('data-door-number'), 10);
             var doorImage = this
+            
+            var lostscreen = document.getElementById('losttitle')
 
             console.log(`Opened the door ${doorNumber} while the bad door is the ${randomnumber}nd one`);
 
             if (doorNumber === randomnumber) {
                 var explosionsound = new Audio('media/explosion.mp3');
                 console.log('You lost!');
+                lostscreen.style.display='block';
                 explosionsound.play()
+                doorImage.src ="media/baddoor.png";
+                for (v=0;v<doors.length;v++) {
+                    if (doors[v] !== doorImage) {
+                        doors[v].style.display='none';
+                        setTimeout(function() {
+                            lostscreen.style.display='none';
+                            for (k=0;k<doors.length;k++) {
+                                doors[k].style.display='inline-block';
+                            }
+                        }, 300);
+                    }
+                }
                 if (currentscore > highscore) {
                   //  window.location.href="gameover.html";
                     highscore = currentscore;
                     
                     highscoreElement.textContent = highscore;
                 }
-                doorImage.src ="media/baddoor.png";
                 currentscore = 0
                 currentscoreElement.textContent = currentscore;
             } else {
@@ -48,7 +62,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 for (i=0;i<doors.length;i++) {
                     doors[i].src = 'media/doorplaceholder.png'
                 }
-                console.log("This code will be executed after 3 seconds.");
             }, 500);
         });
     }
